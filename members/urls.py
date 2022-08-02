d27ac3b31bea3c3028a079ba922d5341554b64cc
enzo11183 <46426033+enzo11183@users.noreply.github.com>
2022-08-02 14:52:46 +0300

    userDel

--- members/urls.py
+++ members/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from . import views
 from .views import UserRegisterView
 
 app_name = 'members'
 urlpatterns = [
     # post views
     path('register/', UserRegisterView.as_view(), name= 'register'),
+    path('delete/', views.userDelete, name = 'delete'),
 
     
 ]
--- members/views.py
+++ members/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from .forms import NewUserForm
+
 
 # Create your views here.
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = NewUserForm
     template_name = 'registration/registration.html'
     success_url = '/members/login'
+    
+
+def userDelete(request):
+    
+    u = request.user
+    if u.is_anonymous:
+        return render(request, 'registration/error.html')
+    else:
+        u.delete()
+        return render(request, 'registration/userdeleted.html')
